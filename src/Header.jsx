14124eef4b6ab3148c4690f4fb6c2b12d973bc4b
dev-nahian <nahian.bdcallin@gmail.com>
2025-01-07 18:16:54 +0600

    SetUp 1/7

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from './assets/logo.svg'
 import Rign from './assets/ring.svg'
 import Moon from './assets/icons/moon.svg'
 import ShoppingCart from './assets/shopping-cart.svg'
+import CartDetails from "./cine/CartDetails";
 
 
 
 export default function Header() {
+
+  const [showCart, setShowCart] = useState(false)
+
+  function handleCartShow(){
+    setShowCart(true)
+  }
+
+
   return (
     <header>
+      {
+        showCart && <CartDetails onClose={()=> setShowCart(false)} />
+      }
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={logo} width="139" height="26" alt="" />
@@ -40,6 +52,7 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={handleCartShow}
             >
               <img
                 src={ShoppingCart}
